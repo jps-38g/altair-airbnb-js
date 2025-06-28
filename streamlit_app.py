6d16404b7b32644aa822168d29fff1a0095d56c8
jps-38g <schorr@bc.edu>
2025-06-28 18:21:23 +0000

    First bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,21 @@ import altair as alt
 import pandas as pd
 
 # Read the .csv file
-
 df = pd.read_csv("listings.csv")
 
-# Display the first few rows of the dataframe
-st.title("Displaying Pandas head() in Streamlit")
 
-st.subheader("Using st.dataframe()")
-st.dataframe(df.head())
+st.set_page_config(page_title="Interactive Dashboard", layout="wide")
+
+# Display the first few rows of the dataframe
+#st.title("Displaying Pandas head() in Streamlit")
+
+#st.subheader("Using st.dataframe()")
+#st.dataframe(df.head())
+
+bar_chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X('neighbourhood_cleansed').sort('-y'),
+    y=alt.Y('count()', title='Available Property Count'),
+    color=alt.Color("count()", scale=alt.Scale(scheme="greens"))
+    )
+
+st.altair_chart(bar_chart, use_container_width=False)
