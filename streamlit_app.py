95a824602c8a58f81d7ddb9a4b132de0fca0e05d
jps-38g <schorr@bc.edu>
2025-06-28 17:44:35 +0000

    Initial libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
+# app.py
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import altair as alt
+import pandas as pd
