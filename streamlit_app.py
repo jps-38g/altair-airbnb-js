078e42481a3659b60fcabb07a2042a6dec2c9189
jps-38g <schorr@bc.edu>
2025-06-28 17:58:14 +0000

    Testing head

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,13 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+
+# Read the .csv file
+
+df = pd.read_csv("listings.csv")
+
+# Display the first few rows of the dataframe
+st.title("Displaying Pandas head() in Streamlit")
+
+st.subheader("Using st.dataframe()")
+st.dataframe(df.head())
