037eea42e2b583df758d85f759695e5349d82d88
jps-38g <schorr@bc.edu>
2025-06-29 14:54:13 +0000

    New Layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,27 @@ df['availability_365_bin'] = pd.cut(df['availability_365'], bins=[0, 50, 100, 15
 
 
 # Create a New Character Column Describing Bin Ranges of Review Score Ratings
-df['review_scores_rating_bin'] = pd.cut(df['review_scores_rating'], bins=[0, 3, 3.5, 4, 4.5, 4.75, 5],
-                                    labels=['0 to 3','3 to 3.5', '3.5 to 4', '4 to 4.5', '4.5 to 4.75', '4.75 to 5'])   
+df['review_scores_rating_bin'] = pd.cut(df['review_scores_rating'], bins=[0, 3, 3.5, 4, 4.5, 4.75, 4.99, 5],
+                                    labels=['0 to 3','3 to 3.5', '3.5 to 4', '4 to 4.5', '4.5 to 4.75', '4.75 to 4.99', '5'])   
     
 
 st.cache_data.clear()
 
+#Convert the 'review_scores_rating_bin' column to a categorical type
+df['review_scores_rating_bin'] = df['review_scores_rating_bin'].cat.add_categories('Missing').fillna('Missing')
+df['review_scores_rating_bin'] = df['review_scores_rating_bin'].astype('category')
+
+# Creating a list of rating bins for the selectbox using the unique values from the DataFrame
+rating_bins = df['review_scores_rating_bin'].unique().tolist()
+
+# Sorting the rating bins
+rating_bins.sort()
+
 # Selectbox: Filter by Rating
-rating = st.selectbox("Filter by Property Rating", ["All"] + list(df["review_scores_rating_bin"].unique()))
+# rating = st.selectbox("Filter by Property Rating", ["All"] + list(df["review_scores_rating_bin"].unique()))
+
+rating = st.selectbox("Filter by Property Rating", ["All"] + rating_bins)
+
 
 # Apply filters
 filtered_props = df if rating == "All" else df[(df["review_scores_rating_bin"] == rating)]
@@ -28,7 +41,7 @@ filtered_props = df if rating == "All" else df[(df["review_scores_rating_bin"] =
 select_neighborhood = alt.selection_point(fields=['neighbourhood_cleansed'], on='click', empty='all')
 
 # First chart: Bar chart of available properties by neighbourhood
-st.title("Airbnb Available Listings")
+st.title("Airbnb Available Listings By Neighbourhood")
 bar_chart = alt.Chart(filtered_props).mark_bar().encode(
     x=alt.X('neighbourhood_cleansed').sort('-y'),
     y=alt.Y('count()', title='Available Property Count'),
@@ -45,7 +58,7 @@ bar_chart = alt.Chart(filtered_props).mark_bar().encode(
 bar_chart2 = alt.Chart(filtered_props, title="Bar Chart:  Property Counts By Host Response Time Bin").mark_bar().encode(
     y=alt.Y('host_response_time', title='Host Response Time').sort('-x'),
     x=alt.Y('count()', title='Property Count'),
-    color=alt.Color("count()", scale=alt.Scale(scheme="greens"))
+    color=alt.Color("count()", scale=alt.Scale(scheme="blues"))
 ).transform_filter(
     select_neighborhood
     )
@@ -56,7 +69,7 @@ bar_chart2 = alt.Chart(filtered_props, title="Bar Chart:  Property Counts By Hos
 bar_chart3 = alt.Chart(filtered_props, title="Bar Chart: Property Counts By # of Days Available In Last Year").mark_bar().encode(
     y=alt.Y('availability_365_bin', title='# of Days Available in Last Year').sort('-x'),
     x=alt.Y('count()', title='Property Count'),
-    color=alt.Color("count()", scale=alt.Scale(scheme="greens"))
+    color=alt.Color("count()", scale=alt.Scale(scheme="blues"))
  ).transform_filter(
     select_neighborhood
     )
@@ -64,4 +77,7 @@ bar_chart3 = alt.Chart(filtered_props, title="Bar Chart: Property Counts By # of
 #st.altair_chart(bar_chart3, use_container_width=False)
 
 # Combine and display the charts
-st.altair_chart(alt.vconcat(bar_chart, bar_chart2, bar_chart3),use_container_width=False)
+
+bottom_charts = bar_chart2 | bar_chart3
+
+st.altair_chart(alt.vconcat(bar_chart, bottom_charts),use_container_width=False)
